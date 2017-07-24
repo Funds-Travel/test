2a93ad98bf21879ff4d8812abe584ee0dace4cfc
Sean <sean@Sens-MacBook-Pro.local>
2017-07-24 16:18:52 -0500

    created folder structure for the backend

---
+++ fundstravel/server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const app = express();
+
+app.use(express.static(__dirname + '/build'))
+
+app.get('/api/test', function(req, res, next){
+    res.send('testing !!')
+})
+
+app.listen(4000, function(){
+    console.log("listening on port 4000")
+})
--- fundstravel/src/index.js
+++ fundstravel/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios'
 
+axios.get('/api/test')
+.then(response => {console.log(response)})
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
