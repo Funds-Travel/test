92b2ebf20171f40b897e53415359b24fdd9ae7d2
Brobbiethao <Brobbiethao@gmail.com>
2017-07-25 15:29:11 -0500

    serverinit_v1

--- fundstravel/server/server.js
+++ fundstravel/server/server.js
@@ -1,15 +1,25 @@
+// require('dotenv').config()
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const connectionString = require()
 const app = express();
+const massive = require('massive');
+const connectionString = require('../config')
+const config = require('../config')
 
-app.use(express.static(__dirname + '/build'))
+app.use(express.static(__dirname + '/build'));
+
+
+massive(connectionString)
+.then(function(dbInstance) {
+    app.set('db', dbInstance)
+});
 
 // app.get('/api/test', function(req, res, next){
 //     res.send('testing !!')
 // })
 
-app.listen(4000, function(){
-    console.log("listening on port 4000")
-})
+app.listen(4000, () => {
+    console.log("listening from Server")
+});
