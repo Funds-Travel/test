fc0a1b6f85e887590ae7daa94c07c2479042d959
Brobbiethao <Brobbiethao@gmail.com>
2017-07-25 10:34:20 -0500

    serverstart

--- fundstravel/server/server.js
+++ fundstravel/server/server.js
@@ -1,10 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const connectionString = require()
+const massive = require('massive')
+
+
+
+const server = app.listen(process.env.PORT || 3001, function() {
+    const port = server.address().port);
+    console.log("Server is listening on port", port);
+})
+
+const dbConfig = {
+    user: "bxgewkyr",
+    password: "6dGlWeNrRIaXjciK5SnFrw43C8tf3Mkz",
+    server: "stampy.db.elephantsql.com",
+    database: "DM6-Funds_Travel"
+}
+
+const executQuery = function(res, query) {
+    sql.connect(dbConfig, function (err) {
+        if (err) {
+            console.log("Error while connection database:- " + err);
+            res.send(err);
+        }
+        else {
+            res.send(err);
+        }
+    });
+}
+
+
+
 const app = express();
 
 app.use(express.static(__dirname + '/build'))
+
+massive(connectionString)
+.then(dbInstance => {
+    app.set('db', dbInstance)
+})
 
 // app.get('/api/test', function(req, res, next){
 //     res.send('testing !!')
