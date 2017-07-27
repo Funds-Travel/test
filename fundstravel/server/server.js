6af606f3af4317b09bfa8d75411cb167a39405e5
Daniel Hogeland <dhogeland@Daniels-MacBook-Pro.local>
2017-07-27 10:43:55 -0500

    added comments for clarification

--- fundstravel/server/server.js
+++ fundstravel/server/server.js
@@ -1,12 +1,15 @@
 // require('dotenv').config()
 
 const express = require('express');
+// for localAuth
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const expressSession = require('express-session');
+
+
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const massive = require('massive');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 const config = require('../config');
 const serverCtrl = require('./serverCtrl');
 const app = express();
@@ -28,10 +31,13 @@ app.use(expressSession({secret: config.sessionSecret,
 app.use(passport.initialize())
 app.use(passport.session())
 
+// for localAuth logout------ need to set up
 app.get('/logout', function(req, res) {
   req.logout();
   res.redirect('/');
 })
+
+// this is for localAuth login-------POST
 app.post('/welcome', function(req, res, next) {
   passport.authenticate('local', function(err, user, info) {
     if (err) {
@@ -47,18 +53,22 @@ app.post('/welcome', function(req, res, next) {
   })(req, res, next);
 });
 
+// setting the fields to match the sql table
 passport.use(new LocalStrategy({
   usernameField: 'email',
   passwordField: 'password',
   session: false
 },
+// calling to database to check for authorization
   function(email, password, done) {
     console.log(email)
     db.findOne(email)
     .then(function(users) {
+      // if no user--------- user not found
       if (!users.length) {
         return done(null, false, {message: 'User not found!'});
    }  else if (users[0].password !== password) {
+     // if bad password---------------- password not valid
      return done(null, false, {message: 'Password not valid!'});
    }  delete users[0].password;
     const user = users[0];
