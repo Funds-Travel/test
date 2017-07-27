0191b82986878734b1181000764abfa03269e15f
Sean <sean@Sens-MacBook-Pro.local>
2017-07-27 15:06:40 -0500

    display packages completed

--- fundstravel/src/components/Packages/Packages.js
+++ fundstravel/src/components/Packages/Packages.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
-// import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 import './Packages.css';
 
- export default class Packages extends Component {
+export default class Packages extends Component {
+
+    constructor(){
+    	super();
+
+    	this.state = {packages: [] };
+    }
+    componentWillMount() {
+      axios.get('/api/packages').then( response => {
+        console.log(response.data);
+        this.setState({packages: response.data}, () => {console.log(this.state)})
+      })
+    }
 
     render() {
-      return (
-            <div className="packageDiv">
-              This is where the packages will go...
 
+        const myPackages = this.state.packages.map(item => {
+          return (
+            <div className="packageDiv" key={item.id}>
+              <li >
+                {item.hotel_name}
+              </li>
             </div>
+          )
+        })
+
+      return (
+          <ul>{myPackages}</ul>
       )
     }
 }
