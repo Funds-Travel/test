db44b75e89cd3dfa1284ab4d9f44e63e1a028890
Garrett <zerocool@Garretts-MacBook-Pro.local>
2017-07-28 18:00:36 -0500

    fixing stuff

--- fundstravel/src/components/Package/Package.js
+++ fundstravel/src/components/Package/Package.js
@@ -3,17 +3,15 @@ import './Package.css';
 
 export default function Package({item}) {
   return (
-    <div className="eachPackageDiv">
-    <img src={item.hotel_image} alt={item.hote_name}/>
-
+    <div className="eachPackageDiv" key={item.id}>
+ <img className ="" src={item.hotel_image} alt={item.hotel_name}/>
       <ul>
       <li >
         {item.hotel_name}<br />
-        {item.hotel_location}
       </li>
-      <li>
-       Total Price of ${item.total_price} for {item.travelers} traveler(s)<br />
-      <span className="descriptSpan">{item.hotel_description}</span>
+      <li className="price">
+       Total Price of ${item.total_price} for {item.travelers} traveler(s)
+       <br /><br />
       </li>
       </ul>
     </div>
