0485ea7328c2daa5998c37215e87c6f240f13fb1
Sean <sean@Sens-MacBook-Pro.local>
2017-08-03 11:18:34 -0500

    responsive

--- fundstravel/src/components/Package/Package.jsx
+++ fundstravel/src/components/Package/Package.jsx
@@ -5,21 +5,41 @@ import './Package.css'
 //need to make sure item is the correct reference...
 export default function Package({item}) {
   return (
-    <div className= "eachPackageDiv"key={item.id}>
+    <div className="col m4">
 
- <img className ="" src={item.hotel_image} alt={item.hotel_name}/>
-      <ul>
-        <li>
-          <div className="cityCountry"> {item.city_country} </div>
-        </li>
-      <li >
-        {item.hotel_name}<br />
-      </li>
-      <li className="price">
-       Total Price of ${item.total_price} <br /> {item.travelers} travelers
-       <br /><br /><br />
-      </li>
-      </ul>
+        <div className="card hoverable" key={item.id}>
+          <ul>
+            <li>
+              <div className="card-image">
+                <img className="responsive-img" src={item.hotel_image} alt={item.hotel_name}/>
+
+              
+              </div>
+              <div className="card-content">
+                <p>{item.hotel_name} <br /> Total Price of ${item.total_price} <br /> {item.travelers} travelers</p>
+              </div>
+
+            </li>
+          </ul>
+        </div>
+
     </div>
   )
 }
+
+  // <div className= "eachPackageDiv"key={item.id}>
+  //
+  // <img className ="" src={item.hotel_image} alt={item.hotel_name}/>
+  //   <ul>
+  //     <li>
+  //       <div className="cityCountry"> {item.city_country} </div>
+  //     </li>
+  //   <li >
+  //     {item.hotel_name}<br />
+  //   </li>
+  //   <li className="price">
+  //    Total Price of ${item.total_price} <br /> {item.travelers} travelers
+  //    <br /><br /><br />
+  //   </li>
+  //   </ul>
+  // </div>
