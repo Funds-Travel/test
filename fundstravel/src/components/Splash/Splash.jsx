c36efec9e4de38f14b48fa421164f175d5c93a43
Brobbiethao <Brobbiethao@gmail.com>
2017-08-06 00:12:45 -0500

    completed

--- fundstravel/src/components/Splash/Splash.jsx
+++ fundstravel/src/components/Splash/Splash.jsx
@@ -111,10 +111,10 @@ signIn() {
          <div className="view2">
            <h3 className="about">
 We started this project with the idea that the Travel industry has an actual niche that remains untapped. Our target demographic was the untraveled, the grandparent, the technically clumsy or easily stressed consumer that could not digest the massive options they were presented when browsing for travel.
-
-Blasphemy? Not quite. Travel agencies have existed for centuries because consumers that could afford to, subscribe to the simplicity of travel options offered.
-
-Our app, although not specifically catered to that crowd is actually designed to perform the same exact functions. Whats more, we have designed it specifically for the wishful consumer, allowing them to put funds aside and visually see what they can afford. 
+<br />
+<br />Blasphemy? Not quite. Travel agencies have existed for centuries because consumers that could afford to, subscribe to the simplicity of travel options offered.
+<br />
+<br />Our app, although not specifically catered to that crowd is actually designed to perform the same exact functions. Whats more, we have designed it specifically for the wishful consumer, allowing them to put funds aside and visually see what they can afford. 
           </h3>
             <div>
               <br />
